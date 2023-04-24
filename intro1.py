1c87ce5458eec190a56f643df9704cafdae9b819
lauradanielaballenbernal <2423lauradaniela@gmail.com>
2023-04-24 18:47:15 +0200

    repaso python

--- intro1.py
+++ intro1.py
@@ -1,1 +1,11 @@
-print('hola mundo')
+i=1
+sum=0
+while i<=10:
+     print (i)
+     sum+=i
+     i+=1
+     print=('la suma es',sum) 
+     pares(2+4+6+8)
+     print ('la suma de los pares es', pares)
+     impar(1+3+5+7+9)
+     print=('la suma de los impares es',impar)
