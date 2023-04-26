24bf3c897a0b37565709493273b30cb1d28944fc
lauradanielaballenbernal <2423lauradaniela@gmail.com>
2023-04-26 15:52:26 +0200

    ejemplo de while

---
+++ while.py
@@ -0,0 +1,9 @@
+x=5
+y=3
+flag=False
+while not (x%y==0 or y%x==0):
+    print('rutina para saber si dos numeros son factor')
+    x= int(input('ingrese numero'))
+    y= int(input('ingrese numero'))
+
+print('son factor')
