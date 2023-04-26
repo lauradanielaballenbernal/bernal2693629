75d544d20fcce530a649d4fd71c07aad9786cbea
lauradanielaballenbernal <2423lauradaniela@gmail.com>
2023-04-26 18:27:14 +0200

    actividad terminada

--- while3.py
+++ while3.py
@@ -1,12 +1,22 @@
 num = 1
 cont = 0
 suma = 0
-
+mayor=0
+menor=1000000
 while num != 0:
   num = int(input('Ingrese número: '))
   cont += 1
   suma += num
+  if num>mayor:
+     mayor=num
+  if num<menor and num!=0:
+     menor=num    
 
-print(f'El usuario ingresó {cont} números')
+print(f'El usuario ingresó {cont-1} números')
 print('La suma de los números es:', suma)
+print(f'El promedio es {suma/(cont-1)} números')
+print(f'El mayor es {mayor}')
+print(f'el menor es´{menor}' )
+
+
 
