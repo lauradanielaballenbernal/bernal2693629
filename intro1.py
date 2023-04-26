25bfc51329c11a6a3201975d1e9e4e69ac03cfe3
lauradanielaballenbernal <2423lauradaniela@gmail.com>
2023-04-26 18:14:02 +0200

    actividad imcompleta corregir

--- intro1.py
+++ intro1.py
@@ -4,8 +4,8 @@ while i<=10:
      print (i)
      sum+=i
      i+=1
-     print=('la suma es',sum) 
-     pares(2+4+6+8)
-     print ('la suma de los pares es', pares)
-     impar(1+3+5+7+9)
+     print('la suma es',sum) 
+     pares=(2+4+6+8)
+     print('la suma de los pares es', pares)
+     impar=(1+3+5+7+9)
      print=('la suma de los impares es',impar)
