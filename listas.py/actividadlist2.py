8edaee1245d030559aed8b1292fbf3b154d4b146
lauradanielaballenbernal <2423lauradaniela@gmail.com>
2023-05-03 11:00:52 -0500

    actividad 1.1

--- listas.py/actividadlist2.py
+++ listas.py/actividadlist2.py
@@ -1,19 +1,22 @@
 import random
 lista=[]
 tam=int(random.randint(10,20))
+sum=0
+mayor=0
+menor=1000000
 print(tam)
 for i in range(tam):
     num=int(random.randrange(100))
     lista.append(num)
+    sum+=num
+    if num>mayor:
+        mayor=num
+    if num<menor and num!=0:
+        menor=num
 print(lista)
 
-suma= sum(lista)
-mayor= max(lista)
-menor=min(lista)
-cont=len(lista)
-
-print(f"el usuario ingreso {cont-1} numeros")
-print(f"la suma es {suma}")
-print(f"el promedio es{suma/(cont-1)} numeros")
+print(f"el usuario ingreso {tam} numeros")
+print(f"la suma es {sum}")
+print(f"el promedio es{sum/(tam)} numeros")
 print(f"el mayor es {mayor}")
 print(f"el menor es {menor}")
