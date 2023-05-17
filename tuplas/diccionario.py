0fbd24d8a5c5194cc9939d1605edcdb99cd7121c
lauradanielaballenbernal <2423lauradaniela@gmail.com>
2023-05-17 11:15:29 -0500

    actividad deccionario

---
+++ tuplas/diccionario.py
@@ -0,0 +1,17 @@
+diccionary={"amarrillo": "yellow",
+            "verde": "green",
+            "morado": "purple",
+            "azul": "blue",
+            "cafe":"brown",
+            "gris": "gray",
+            "naranga": "orange",
+            "rosado":"pink",
+            "rojo": "red",
+            "negro": "black"}
+colors=["rojo", "negro" , "blue" ,"yellow"]
+
+for color in colors:
+    if color in diccionary:
+        print(color, "es", diccionary[color])
+    else:
+        print(color, "no esta en el diccionario")
